cd5bfd8046586eae3f325e465dbf4cbaec33a826
Jason Leong <leody010@mymail.unisa.edu.au>
2025-04-15 11:25:48 +0930

    Added initial files

---
+++ src/components/Hero.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+              {/*passing in title & subtitle prop for dynamic */}
+const Hero = ({
+    title = 'Become a React Dev', 
+    subtitle = 'Find the react job That Fits your skill set'}) => {
+  return (
+    <section className="bg-indigo-700 py-20 mb-4">
+        <div
+          className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center"
+        >
+          <div className="text-center">
+            <h1
+              className="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl"
+            >
+              {title} {/* This is a prop which is Dynamic based on Title in App.jsx */}
+            </h1>
+
+            <p className="my-4 text-xl text-white">
+              {subtitle} {/* This is a prop which is Dynamic based on Title in App.jsx */}
+            </p>
+          </div>
+        </div>
+      </section>
+  )
+}
+
+export default Hero
